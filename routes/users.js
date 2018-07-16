79d23dc7c97511c96ff5ce7701409fa17de3c73e
Richard Tom <tomrichard14@gmail.com>
2018-07-16 13:20:43 -0700

    validation for /user endpoints

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,72 @@ const router = express.Router();
 
 router.post('/', (req, res, next) => {
 
-  // console.log('working');
-  const {fullname, username, password} = req.body;
+  // Missing field
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    const err = new Error(`Missing '${missingField}' in request body`);
+    err.status = 422;
+    return next(err);
+  }
+
+  // Require string type
+  const stringFields = ['username', 'password', 'fullname'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    const err = new Error(`Field: '${nonStringField}' must be type String`);
+    err.status = 422;
+    return next(err);
+  }
+
+  // No whitespace before and after
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with whitespace`);
+    err.status = 422;
+    return next(err);
+  }
+
+  // Ensures character lengths
+  const sizedFields = {
+    username: { min: 1 },
+    password: { min: 8, max: 72 }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  if (tooSmallField) {
+    const min = sizedFields[tooSmallField].min;
+    const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooLargeField) {
+    const max = sizedFields[tooLargeField].max;
+    const err = new Error(`Field: '${tooSmallField}' must be at most ${max} characters long`);
+    err.status = 422;
+    return next(err);
+  }
+
+  // Username and password were validated as pre-trimmed
+  let { username, password, fullname = '' } = req.body;
+  fullname = fullname.trim();
 
   return User.hashPassword(password)
     .then(digest => {

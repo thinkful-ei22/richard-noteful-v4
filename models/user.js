4b483b2f948e9bfb411396d1a83ca597fe062d08
Richard Tom <tomrichard14@gmail.com>
2018-07-16 11:37:38 -0700

    user schema and model created

--- models/user.js
+++ models/user.js
@@ -7,3 +7,14 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true},
   password: String
 });
+
+userSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+    delete ret.password;
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);

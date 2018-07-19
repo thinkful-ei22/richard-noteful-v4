0975d79db6f45eab5fbb186b0194180a9bc1886e
Richard Tom <tomrichard14@gmail.com>
2018-07-19 11:47:00 -0700

    switched to mlab port

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
-  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user1:ASDF1234@ds151180.mlab.com:51180/noteful-v4',
+  // MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user1:ASDF1234@ds151180.mlab.com:51180/noteful-v4',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
